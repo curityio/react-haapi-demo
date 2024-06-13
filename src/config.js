c69f28e155d7bd73d46deea48ae90e8ba0a6b922
Daniel Lindau <d.lindau@gmail.com>
2024-06-13 12:39:27 +0200

    ADd spec

--- src/config.js
+++ src/config.js
@@ -18,8 +18,8 @@ const config = {
     clientId: 'react-client',
     redirectUri: 'http://localhost:3000/',
     scope: 'openid',
-    authorizationEndpoint: 'https://localhost:8443/oauth/v2/oauth-authorize',
-    tokenEndpoint: 'https://localhost:8443/oauth/v2/oauth-token'
+    authorizationEndpoint: 'https://localhost:8443/dev/oauth/authorize',
+    tokenEndpoint: 'https://localhost:8443/dev/oauth/token'
 }
 
 export default config
