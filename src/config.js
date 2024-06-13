8b821e585dbc720e3e397d8e732ea4b554f6aaba
Daniel Lindau <d.lindau@gmail.com>
2024-06-13 12:52:12 +0200

    Default config

--- src/config.js
+++ src/config.js
@@ -19,7 +19,7 @@ const config = {
     redirectUri: 'http://localhost:3000/',
     scope: 'openid',
     authorizationEndpoint: 'https://localhost:8443/oauth/v2/oauth-authorize',
-    tokenEndpoint: 'https://localhost:8443/oauth/v2/oauth-authorize'
+    tokenEndpoint: 'https://localhost:8443/oauth/v2/oauth-token'
 }
 
 export default config
